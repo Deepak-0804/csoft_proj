0472699b7c5d6f2707de33d1543e6c53fbe19de4
deepakchowdaryvemulapalli <deepakchowdary.vemulapalli@chandusoft.com>
2025-11-17 16:56:18 +0530

    new one

--- app/db.php
+++ app/db.php
@@ -1,6 +1,9 @@
 <?php
 $config = require_once __DIR__ . '/config.php';
 
+var_dump($config);
+exit;
+
 try {
     $pdo = new PDO("mysql:host={$config['db_host']};dbname={$config['db_name']}", $config['db_user'], $config['db_pass']);
     // Set the PDO error mode to exception
