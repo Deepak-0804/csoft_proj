ffc1dccd034cc5533829d708a8ff160717dcd61d
deepakchowdaryvemulapalli <deepakchowdary.vemulapalli@chandusoft.com>
2025-11-19 18:21:58 +0530

    version 12

--- app/db.php
+++ app/db.php
@@ -1,15 +1,19 @@
 <?php
 
-// Use the already-loaded config
 $config = $GLOBALS['config'];
 
 try {
-    $pdo = new PDO(
-        "mysql:host={$config['db_host']};port={$config['db_port']};dbname={$config['db_name']}",
-        $config['db_user'],
-        $config['db_pass']
-    );
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $dsn = "mysql:host={$config['db_host']};port={$config['db_port']};dbname={$config['db_name']};charset=utf8mb4";
+
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::MYSQL_ATTR_SSL_CA => null,
+        PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+    ];
+
+    $pdo = new PDO($dsn, $config['db_user'], $config['db_pass'], $options);
+
+    return $pdo;
 
 } catch (PDOException $e) {
     die("Could not connect: " . $e->getMessage());
