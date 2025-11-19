105c38eb0082baa6d488fafd68f9afceba2a86ac
deepakchowdaryvemulapalli <deepakchowdary.vemulapalli@chandusoft.com>
2025-11-19 18:06:10 +0530

    version 11

--- public/test-db.php
+++ public/test-db.php
@@ -1,4 +1,18 @@
 <?php
+
 require __DIR__ . '/../app/auth.php';
 
-var_dump($GLOBALS['pdo']);
+echo "<pre>";
+
+try {
+    // Show the PDO object type
+    var_dump($GLOBALS['pdo']);
+
+    // Try a simple query to confirm DB connection
+    $stmt = $GLOBALS['pdo']->query("SELECT 1");
+    echo "\nQuery OK: SELECT 1 returned: ";
+    var_dump($stmt->fetch());
+
+} catch (Throwable $e) {
+    echo "\nERROR: " . $e->getMessage();
+}
